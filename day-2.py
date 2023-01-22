2e06a8b7890fb640591dfd54dc2a3dee1bf7ca07
gregorri <qgrisa@gmail.com>
2023-01-22 14:31:10 +0100

    finish day 2

--- day-2.py
+++ day-2.py
@@ -9,9 +9,24 @@
 #second_digit = int(two_dgit_number[1])
 #print(first_digit + second_digit)
 
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+#height = input("enter your height in m: ")
+#weight = input("enter your weight in kg: ")
 
-bmi = int(weight) / float(height) ** 2
+#bmi = int(weight) / float(height) ** 2
 
-print(int(bmi))
+#print(int(bmi))
+
+#age = input("What is your current age? ")
+#age_as_int = int(age)
+#years_remaining = 90 - age_as_int
+#days_remaining = years_remaining * 365
+#weeks_remaining = years_remaining * 52
+#print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {years_remaining} years left.")
+
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+bill_per_person = round((total_bill * (1 + tip / 100)) / people, 2)
+bill_per_person = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay: ${bill_per_person}")
