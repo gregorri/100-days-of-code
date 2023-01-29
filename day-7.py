2759cf5db115f37e987aa7499cbfe083f259eaa0
gregorri <qgrisa@gmail.com>
2023-01-29 23:57:01 +0100

    update

--- day-7.py
+++ day-7.py
@@ -3,18 +3,31 @@ import string
 
 word = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word)
+word_length = len(chosen_word)
+
 print(chosen_word)
 
 display = []
-word_length = len(chosen_word)
-for letter in range(word_length):
+for _ in range(word_length):
     display += "_"
+
 guess = input("Guess a letter: ").lower()
 
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+guessed_letters = False
+while not guessed_letters:
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if guess == "quit":
+        break
+    if "_" not in display:
+        guessed_letters = True
+        print("You win.")
+    else:
+        guess = input("Guess a letter: ").lower()
+        
 
 print(display)
