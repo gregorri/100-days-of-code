5d21bb953618ba52b4c0dc3a292f1785c6854793
gregorri <qgrisa@gmail.com>
2023-02-09 13:21:39 +0100

    Function Parameters and Caesar Cipher

--- day-8/day-8.py
+++ day-8/day-8.py
@@ -26,32 +26,32 @@
 
 # n = int(input("Check this number: "))
 # prime_checker(number=n)
-from art import logo
+# from art import logo
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def caesar(start_text, shift_amount, cipher_direction):
-    end_text = ""
-    if cipher_direction == "decode":
-        shift_amount *= -1
-    for char in start_text:
-        if char in alphabet:
-            position = alphabet.index(char)
-            new_position = (position + shift_amount) % len(alphabet)
-            end_text += alphabet[new_position]
-        else:
-            end_text += char
-    print(f"The {cipher_direction}d text is {end_text}")
+# def caesar(start_text, shift_amount, cipher_direction):
+#     end_text = ""
+#     if cipher_direction == "decode":
+#         shift_amount *= -1
+#     for char in start_text:
+#         if char in alphabet:
+#             position = alphabet.index(char)
+#             new_position = (position + shift_amount) % len(alphabet)
+#             end_text += alphabet[new_position]
+#         else:
+#             end_text += char
+#     print(f"The {cipher_direction}d text is {end_text}")
 
-print(logo)
-#figure out a way to ask the user if they want to restart the cipher program
-while True:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
-    text = input("Type your message: ").lower()
-    shift = int(input("Type the shift number: "))
-    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'. ")
-    if restart == "no":
-        print("Goodbye")
-        break
+# print(logo)
+# #figure out a way to ask the user if they want to restart the cipher program
+# while True:
+#     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+#     text = input("Type your message: ").lower()
+#     shift = int(input("Type the shift number: "))
+#     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+#     restart = input("Type 'yes' if you want to go again. Otherwise type 'no'. ")
+#     if restart == "no":
+#         print("Goodbye")
+#         break
 
