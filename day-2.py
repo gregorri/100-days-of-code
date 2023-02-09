6bbf70e006cf6989eab59a816583bc7dbd4c1da6
gregorri <qgrisa@gmail.com>
2023-02-09 22:41:18 +0100

    initial commit

--- day-2.py
+++ day-2.py
@@ -1,27 +1,27 @@
-#print("hello"[4])
+# print("hello"[4])
 
-#num_char = len(input("What is your name? "))
-#new_num_char = str(num_char)
-#print("Your name has " + new_num_char + " characters.")
+# num_char = len(input("What is your name? "))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
 
-#two_dgit_number = input("Type a two digit number: ")
-#first_digit = int(two_dgit_number[0])
-#second_digit = int(two_dgit_number[1])
-#print(first_digit + second_digit)
+# two_dgit_number = input("Type a two digit number: ")
+# first_digit = int(two_dgit_number[0])
+# second_digit = int(two_dgit_number[1])
+# print(first_digit + second_digit)
 
-#height = input("enter your height in m: ")
-#weight = input("enter your weight in kg: ")
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
 
-#bmi = int(weight) / float(height) ** 2
+# bmi = int(weight) / float(height) ** 2
 
-#print(int(bmi))
+# print(int(bmi))
 
-#age = input("What is your current age? ")
-#age_as_int = int(age)
-#years_remaining = 90 - age_as_int
-#days_remaining = years_remaining * 365
-#weeks_remaining = years_remaining * 52
-#print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {years_remaining} years left.")
+# age = input("What is your current age? ")
+# age_as_int = int(age)
+# years_remaining = 90 - age_as_int
+# days_remaining = years_remaining * 365
+# weeks_remaining = years_remaining * 52
+# print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {years_remaining} years left.")
 
 # print("Welcome to the tip calculator.")
 # total_bill = float(input("What was the total bill? $"))
