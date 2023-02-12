75247cc1e9a09d417740e046d5bae9aeb7ac0118
gregorri <qgrisa@gmail.com>
2023-02-12 19:31:38 +0100

    OOP Coffee machine and  .csv file

--- day-16/day-16.py
+++ day-16/day-16.py
@@ -1,9 +1,24 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timy = Turtle()
+# screen = Screen()
+# timy.shape("turtle")
+# timy.color("red")
+#
+# print(screen.candlelight)
+# screen.exitonclick()
+from prettytable import PrettyTable
+from prettytable import from_csv
 
-timy = Turtle()
-screen = Screen()
-timy.shape("turtle")
-timy.color("red")
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
 
-print(screen.canvheight)
-screen.exitonclick()
+with open("data.csv") as data_file:
+    data = PrettyTable()
+    data_set = from_csv(data_file)
+    print(data_set)
+    # data = data_file.readlines()
+    # for line in data:
+    #     data_file.add_row(line)
+    #     print(line)
