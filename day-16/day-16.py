b702e93039e5d3f0286960cb1ec8d5f7cf9a1089
gregorri <qgrisa@gmail.com>
2023-02-13 21:15:27 +0100

    FIX* OOP Modifying Object Attributes and Calling Methods

--- day-16/day-16.py
+++ day-16/day-16.py
@@ -10,15 +10,7 @@
 from prettytable import PrettyTable
 from prettytable import from_csv
 
-# table = PrettyTable()
-# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-# table.add_column("Type", ["Electric", "Water", "Fire"])
-
-with open("data.csv") as data_file:
-    data = PrettyTable()
-    data_set = from_csv(data_file)
-    print(data_set)
-    # data = data_file.readlines()
-    # for line in data:
-    #     data_file.add_row(line)
-    #     print(line)
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table)
