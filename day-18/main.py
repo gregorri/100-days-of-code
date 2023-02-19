15a56338cc21dfcca51d7eef3c1308a3f913c733
gregorri <qgrisa@gmail.com>
2023-02-19 23:16:39 +0100

    Draw a Spirograph

--- day-18/main.py
+++ day-18/main.py
@@ -2,28 +2,22 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+screen = t.Screen()
+t.colormode(255)
+tim.pensize(2)
+tim.speed("fastest")
+
 
 def random_color():
-	r = random.random()
-	g = random.random()
-	b = random.random()
+	r = random.randint(0, 255)
+	g = random.randint(0, 255)
+	b = random.randint(0, 255)
 	color = (r, g, b)
 	return color
 
-
-def draw_shape(num_sides):
-	angle = 360 / num_sides
+for i in range(40):
 	tim.color(random_color())
-	for _ in range(num_sides):
-		tim.forward(100)
-		tim.right(angle)
+	tim.circle(100)
+	tim.left(10)
 
-def draw_spirograph(size_of_gap):
-	for _ in range(int(360 / size_of_gap)):
-		tim.color(random_color())
-		tim.circle(100)
-		tim.setheading(tim.heading() + size_of_gap)
-
-for shape_side_n in range(3, 11):
-	draw_shape(shape_side_n)
-
+t.exitonclick()
