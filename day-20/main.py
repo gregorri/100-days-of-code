28c39ff1dd2ac768390cd038408f986924198092
gregorri <qgrisa@gmail.com>
2023-02-22 21:54:27 +0100

    Create a Snake Class & Move to OOP

---
+++ day-20/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
+# Create window screen
+screen = Screen()
+screen.setup(width=500, height=400)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+	screen.update() # update screen
+	time.sleep(0.4) # delay
+
+	snake.move()
+
+
+screen.exitonclick()
