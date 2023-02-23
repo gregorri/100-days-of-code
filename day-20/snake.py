934b3625d969b466293b7b474ada4f6e5a9df90c
gregorri <qgrisa@gmail.com>
2023-02-23 08:31:28 +0100

    How to Control the Snake with a Keypress

--- day-20/snake.py
+++ day-20/snake.py
@@ -9,6 +9,7 @@ class Snake:
 		self.create_snake()
 
 	def create_snake(self):
+		"""Create the snake body"""
 		for position in STARTING_POSITIONS:
 			new_segment = Turtle("square")
 			new_segment.color("white")
@@ -17,8 +18,23 @@ class Snake:
 			self.all_segments.append(new_segment)
 
 	def move(self):
+		"""Move the snake forward by 20 pixels"""
 		for seg_num in range(len(self.all_segments) - 1, 0, -1):
 			new_x = self.all_segments[seg_num - 1].xcor()
 			new_y = self.all_segments[seg_num - 1].ycor()
 			self.all_segments[seg_num].goto(new_x, new_y)
 		self.all_segments[0].forward(20)
+
+	def up(self):
+		"""Move the snake up by 20 pixels"""
+		self.all_segments[0].setheading(90)
+
+	def down(self):
+		"""Move the snake down by 20 pyxels"""
+		self.all_segments[0].setheading(270)
+
+	def left(self):
+		self.all_segments[0].setheading(180)
+
+	def right(self):
+		self.all_segments[0].setheading(0)
