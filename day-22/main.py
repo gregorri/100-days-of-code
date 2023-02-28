b0c9a1b99381f00558bd732abb4c97842e37f48a
gregorri <qgrisa@gmail.com>
2023-02-28 23:29:59 +0100

    Add the Scoreboard and Game Over sequence

--- day-22/main.py
+++ day-22/main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -8,7 +8,10 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+
 player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
 # Move the player up when the "Up" key is pressed
 screen.listen()
@@ -20,6 +23,26 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # Add cars to the screen
+    car_manager.create_car()
+    # Move the cars
+    car_manager.move_cars()
+    # Add score board
+    scoreboard.update_scoreboard()
+
+    # Add 1 to the score when the player reaches the finish line
+    if player.finish_line():
+        player.reset()
+        car_manager.level_up()
+        scoreboard.increase_score()
+
+
+    # Detect collision with the cars
+    for car in car_manager.all_cars:
+        if car.distance(player.player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
--- day-22/scoreboard.py
+++ day-22/scoreboard.py
@@ -1,5 +1,32 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="left", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
+    def reset(self):
+        self.score = 0
+        self.clear()
+        self.update_scoreboard()
+
