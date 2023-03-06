e51ea5f692e11732233e56f4405932eaefe222ec
gregorri <qgrisa@gmail.com>
2023-03-06 19:32:23 +0100

    The Great Squirrel Census Data Analysis (with Pandas!)

--- day-25/main.py
+++ day-25/main.py
@@ -1,16 +1,33 @@
-import csv
-
-with open ("weather_data.csv") as data_file:
-	data = csv.reader(data_file)
-	temperatures = []
-	for row in data:
-		if row[1] != "temp":
-			temperatures.append(int(row[1]))
-
-	print(temperatures)
-
-
+# import csv
 import pandas
+#
+# with open("weather_data.csv") as data_file:
+# 	data = csv.reader(data_file)
+# 	temperatures = []
+# 	for row in data:
+# 		if row[1] != "temp":
+# 			temperatures.append(int(row[1]))
+#
+# 	print(temperatures)
+#
+
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+data_dict = data.to_dict()
+temp_list = data["temp"].to_list()
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# Get data in columns
+# print(data["condition"])
+
+# Get maximum temperature in a row
+# print(data[data.temp == data.temp.min()])
+
+# monday = data[data.day == "Monday"]
+# Convert to Fahrenheit
+# monday_temp = int(monday.temp) * 9/5 + 32
+# print(monday_temp)
+
+
+
