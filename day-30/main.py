3f66b210a0f1ec69ddf7846fe1a435757274d4a9
gregorri <qgrisa@gmail.com>
2023-03-13 13:53:44 +0100

    : Exception Handling in the NATO Phonetic Alphabet Project

--- day-30/main.py
+++ day-30/main.py
@@ -1,11 +1,15 @@
-fruits = ["Apple", "Peach", "Pear"]
+import pandas
 
-def make_pie(index):
-	try:
-		fruit = fruits[index]
-	except IndexError:
-		print("Fruit pie")
-	else:
-		print(fruit + " pie")
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-make_pie(1)
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+word = input("Enter a word: ").upper()
+
+while True:
+    try:
+        word_list = [phonetic_dict[letter] for letter in word]
+        print(word_list)
+        break
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        word = input("Enter a word: ").upper()
